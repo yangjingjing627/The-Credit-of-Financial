d496107269d844a5c60bc0e859c51244353f6261
yangjj <yangjinjin@qianbao.com>
2017-11-15 15:34:18 +0800

    UI08-公告，UI09公告详情

--- src/pages/index/views/notice/notice.vue
+++ src/pages/index/views/notice/notice.vue
@@ -3,9 +3,9 @@
         <div class="wrap">
             <!--账单明细  -->
             <div class="billDeail">
-                <div class="flexA flexA_140" @click="billSection(1)">
+                <div class="flexA flexA_140 arrow_r" @click="billSection(1)">
                     <div>
-                        <div class="f30">关于银行渠道升级通知</div>
+                        <div class="f30 g3">关于银行渠道升级通知</div>
                         <div class="f26 g9">2017-08-04 18:14</div>
                     </div>
                 </div>
@@ -23,13 +23,13 @@
                 </div>
                 <div class="wrap mt20 f30">
                     <div class="flexA">
-                        <h5 class="f30">关于银行渠道升级通知</h5>
-                        <div class="f26 g9">2017-08-04 18:14</div>
+                        <h5 class="f30 g3">关于银行渠道升级通知</h5>
+                        <div class="f24 g9">2017-08-04 18:14</div>
                     </div>
                     <div class="flexA flexA_auto">
-                        <div class="f26 g6">
-                            <h5 class="pt20">尊敬的宝粉儿：</h5>
-                            <div class="ti pt40 pb30">
+                        <div class="f28 g6">
+                            <h5 class="pt30">尊敬的宝粉儿：</h5>
+                            <div class="ti pt50 pb30">
                                 中信银行08月05日17:30至08月06日07:00进行系统维护，期间暂停支付业务，请各位宝粉们提前知悉，如有充值需求，请提前做好资金安排，为此给您带来的不便，敬请谅解！
                             </div>
                         </div>
