1c02edc0663e0fa14c1345c2e8d9ada51323b92a
yangjj <yangjinjin@qianbao.com>
2017-11-15 18:49:29 +0800

    36修改资料

--- src/pages/index/views/editProfile/editProfile.vue
+++ src/pages/index/views/editProfile/editProfile.vue
@@ -1,33 +1,33 @@
 <template>
     <div>
-        <ul class="wrap pr30 f30">
+        <ul class="wrap f30">
             <li class="flexA pr10">
-                <div class="iconW">家庭信息</div>
-                <div class="g6 blue iconW pr40 icon8">去修改</div>
+                <div class="iconW icon1">家庭信息</div>
+                <div class="f30 red pr40 icon8">去修改</div>
             </li>
             <li class="flexA pr10">
                 <div class="iconW icon2">营业执照</div>
-                <div class="g6">已修改</div>
+                <div class="g9 f30">已修改</div>
             </li>
             <li class="flexA pr10">
                 <div class="iconW icon3">经营信息</div>
-                <div class="g6">销售待修改</div>
+                <div class="g9 f30">销售待修改</div>
             </li>
             <li class="flexA pr10">
                 <div class="iconW icon4">店铺照片</div>
-                <div class="g6">销售待修改</div>
+                <div class="g9 f30">销售待修改</div>
             </li>
             <li class="flexA pr10">
                 <div class="iconW icon5">规模照片</div>
-                <div class="g6">销售待修改</div>
+                <div class="g9 f30">销售待修改</div>
             </li>
             <li class="flexA pr10">
                 <div class="iconW icon6">许可证</div>
-                <div class="g6">销售待修改</div>
+                <div class="g9 f30">销售待修改</div>
             </li>
             <li class="flexA pr10">
                 <div class="iconW icon7">补充材料</div>
-                <div class="g6">销售待修改</div>
+                <div class="g9 f30">销售待修改</div>
             </li>
         </ul>
         <div class="btn mt60 w670">确认提交</div>
@@ -36,37 +36,49 @@
 <style lang="sass" scoped>
     .iconW {
         padding-left: 67px;
-        background: url("images/editProfile.png") 8px 4px no-repeat;
-        background-size: 36px 358px;
+        font-size: 32px;
+        color: #333;
+    }
+    .icon1 {
+        background: url("images/1.png") 8px 4px no-repeat;
+        background-size: 34px 38px;
     }
 
     .icon2 {
-        background-position: 8px -40px;
+      background: url("images/2.png") 8px 4px no-repeat;
+      background-size: 34px 38px;
     }
 
     .icon3 {
-        background-position: 8px -80px;
+      background: url("images/3.png") 8px 4px no-repeat;
+      background-size: 34px 38px;
     }
 
     .icon4 {
-        background-position: 8px -128px;
+      background: url("images/4.png") 8px 4px no-repeat;
+      background-size: 34px 38px;
     }
 
     .icon5 {
-        background-position: 8px -176px;
+      background: url("images/5.png") 8px 4px no-repeat;
+      background-size: 34px 38px;
     }
 
     .icon6 {
-        background-position: 8px -226px;
+      background: url("images/6.png") 8px 4px no-repeat;
+      background-size: 34px 38px;
     }
 
     .icon7 {
-        background-position: 8px -274px;
+      background: url("images/7.png") 8px 4px no-repeat;
+      background-size: 34px 38px;
     }
 
     .icon8 {
         padding-left: 0;
-        background-position: 112px -318px;
+        background: url("images/8.png") no-repeat right center;
+        background-size: 16px 26px;
     }
+
 </style>
 <script src="./editProfile.js"></script>
