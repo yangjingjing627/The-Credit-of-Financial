9632145a834cd4a5ebe0ee515349c3d93c378418
yangjj <yangjinjin@qianbao.com>
2017-11-16 16:50:30 +0800

    55修改登录密码modifyLogPsw

--- src/pages/index/views/modifyLogPsw/modifyLogPsw.vue
+++ src/pages/index/views/modifyLogPsw/modifyLogPsw.vue
@@ -2,22 +2,22 @@
     <div>
         <ul class="wrap">
             <li class="flexA">
-                <span>原登录密码</span>
+                <span class="f30 g3">原登录密码</span>
                 <input type="password" class="w440 db" v-model="psw1" placeholder="请输入原登录密码">
             </li>
             <li class="flexA">
-                <span>新登录密码</span>
+                <span class="f30 g3">新登录密码</span>
                 <input type="password" placeholder="请设置6-20位登录密码" maxlength="20" class="w440 db" v-model="psw2">
             </li>
             <li class="flexA">
-                <span>确认新密码</span>
+                <span class="f30 g3">确认新密码</span>
                 <input type="password" placeholder="请再次输入新登录密码" maxlength="20" class="w440 db" :class="{confirm:flag}"
                        v-model="psw3">
             </li>
         </ul>
         <p class="f24 g9">密码为6-20位数字、字母或字符组合，不允许有空格</p>
         <div class="content">
-            <div class="btn" :class="psw1&&psw2&&psw3?'':'opacity'" @click="modifyLogPsw">确认提交</div>
+            <div class="btn" :class="psw1&&psw2&&psw3?'':'opacity'" @click="modifyLogPsw">提交</div>
         </div>
     </div>
 </template>
@@ -31,7 +31,7 @@
     }
 
     .f24 {
-        padding: 20px 0 88px 40px;
+        padding: 20px 0 149px 40px;
         line-height: 1;
     }
 
