5e18235d3858f8ed1ffd97ed63117a32ccfabe61
yangjj <yangjinjin@qianbao.com>
2017-11-16 17:11:32 +0800

    53实名认证成功realNameSuccess

--- src/pages/index/views/realNameSuccess/realNameSuccess.vue
+++ src/pages/index/views/realNameSuccess/realNameSuccess.vue
@@ -2,7 +2,7 @@
     <div class="success_wrap">
         <div class="bd tc">
             <div class="pic"></div>
-            <h2 class="title f36">恭喜，您实名认证成功！</h2>
+            <h2 class="title f32 g3">恭喜，您的实名认证已经成功！</h2>
         </div>
         <!--跳转到钱包页  -->
         <div class="content">
@@ -10,5 +10,20 @@
         </div>
     </div>
 </template>
+<style lang="sass" scoped>
+  .success_wrap {
+    padding-top: 200px;
+    margin-top: 20px;
+  }
+  .success_wrap .bd .pic {
+    background: url("./images/success.png") center center no-repeat;
+  }
+  .success_wrap .bd .title {
+    padding-top: 20px;
 
+  }
+  .success_wrap .bd {
+    padding-bottom: 190px;
+  }
+</style>
 <script src="./realNameSuccess.js"></script>
