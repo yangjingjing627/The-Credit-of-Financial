488b11d2cfb30fffdf123b246b8629d826b4f6b0
yangjj <yangjinjin@qianbao.com>
2018-03-21 16:56:20 +0800

    搜索页-背景处理

--- src/pages/index/views/storeSearch/storeSearch.vue
+++ src/pages/index/views/storeSearch/storeSearch.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="bg-white h-100">
-      <div class="bte bbe flexA">
+    <div class="h-100">
+      <div class="bg-white bte bbe flexA">
           <div class="g6 w540 h56"><input class="db input inpSearch pl66 radius100" type="text" placeholder="找优惠点我试试" name="" value=""></div>
           <h5>取消</h5>
       </div>
-      <div class="search-history plr44 bbe">
-        <p class="pt28 pb20 f24 gc pl22">搜索历史</p>
-        <ul class="g6 f28 pl22">
+      <div class="search-history bbe">
+        <p class="bg-white pt28 pb20 f24 gc pl68">搜索历史</p>
+        <ul class="bg-white g6 f28 pl68">
           <li class="ptb20">望京</li>
           <li class="ptb20">渝乡人家</li>
           <li class="ptb20">望京</li>
@@ -30,7 +30,9 @@
           <li class="ptb20">张妈妈</li>
           <li class="ptb20">张妈妈</li>
         </ul>
-        <p class="clear-history tc g6 f24 bte">清除搜索历史</p>
+        <div class="bg-white pb20">
+          <p class="clear-history tc g6 f24 bte">清除搜索历史</p>
+        </div>
       </div>
     </div>
 </template>
@@ -62,41 +64,38 @@
   padding-top: 20px;
   padding-bottom: 20px;
 }
-.plr44 {
-  padding: 0 44px;
+.pl68 {
+  padding-left: 68px;
 }
-.pl22 {
-  padding-left: 22px;
+.radius100 {
+  border-radius: 100px;
 }
-  .radius100 {
+.input {
+  color: #A0A2A7;
+  font-size: 28px;
+  opacity: 0.5;
+  line-height: 56px;
+}
+.inpSearch {
+    background: #F8F8F8 url(./../static/images/icon_search.png) no-repeat 20px center;
+    background-size: 28px 30px;
     border-radius: 100px;
-  }
-  .input {
-    color: #A0A2A7;
-    font-size: 28px;
-    opacity: 0.5;
+    width: 100%;
+    height: 56px;
     line-height: 56px;
-  }
-  .inpSearch {
-      background: #F8F8F8 url(./../static/images/icon_search.png) no-repeat 20px center;
-      background-size: 28px 30px;
-      border-radius: 100px;
-      width: 100%;
-      height: 56px;
-      line-height: 56px;
-      font-size: 28px;
-  }
-  .search-history {
-    height: 100%;
-    overflow: hidden;
-  }
-  ul {
-    height: 76%;
-    overflow: scroll;
-  }
-  .clear-history {
-    padding: 27px 44px 0;
-    margin-bottom: 30px;
-  }
+    font-size: 28px;
+}
+.search-history {
+  height: 100%;
+  overflow: hidden;
+}
+ul {
+  max-height: 76%;
+  overflow: scroll;
+}
+.clear-history {
+  padding: 27px 0px 0;
+  margin: 0 44px;
+}
 </style>
 <script src="./storeSearch.js"></script>
